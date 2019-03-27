dd00bff14b53ce983c81bf5cd64db54c4c6090b5
Gershvin <vchekis@mail.ru>
2019-03-27 20:21:21 +0500

    Переделан костяк. Функция заменена на набор случайных чисел.Сделана функция, считающая интеграл произведения случайных функций

--- Коррелирующие сигналы.py
+++ Коррелирующие сигналы.py
@@ -1,30 +1,32 @@
+"""
+Алгоритм программы:
+1) создание случайного массива, его интерполяция по методу Лагранжа
+2) создание функции на основе интерполяции
+3)
+"""
+
+
 import numpy as np
 import random
-import matplotlib.pyplot as plt
 
-def integral(y1, y2): #функция расчёта интегшралов
+d = 0.1          #шаг численнного интегрирования
+s1 = 0           #интеграл первого полинома
+s2 = 0           #интеграл второго полинома
+
+def integral(y1, y2): #функция расчёта интегралов
     for i in range(a, b):
+        func1 = random.uniform(0, 150)
+        func2 = random.uniform(0, 150)
         s1 = s1 + d * y1*y2
         i = i + d
+        if y1 == y2:
+            return(s1)
+        else:
+            return(s2)
 
-a = np.array([random.random() for i in range(10)])
-print(a)
-plt.plot([1, 2, 3])
-
-a = input(int('введите начало участка анализа'))
-b = input(int('введите конец участка анализа'))
-d = 0.1
-s1 = 0
-s2 = 0
+a = int(input('введите левый предел '))
+b = int(input('введите правый предел '))
 D = 1/(b-a)
-y #значение функции в точке
-for i in range (a, b):
-    s1 = s1+ d*y
-    i = i+d
-
-for i in range(a, b):
-    s1 = s1 + d*y
-    i = i+d
 corr = D*integral(func1, func2) / (abs(D)*(pow(integral(func1, func1)*integral(func2, func2)), 0.5))
 
 
